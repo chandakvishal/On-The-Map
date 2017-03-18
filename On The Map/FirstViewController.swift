e1090c4898fb35a47afb3c7bba673cd6e8be3b80
Vishal Chandak <vcchanda@amazon.com>
2017-03-18 23:23:06 +0530

    Initial Commit

---
+++ On The Map/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  On The Map
+//
+//  Created by Chandak, Vishal on 18/03/17.
+//  Copyright © 2017 Chandak, Vishal. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
